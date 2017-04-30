eed34b651486d3caa8a4db588291737f50be9028
alijinnah <alij@blinds.com>
2017-04-30 17:41:00 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,6 +7,18 @@
 ## makeCacheMatrix returns set and get methods for a matrix. Set calls the R method 'solve' to calculate the inverse of the matrix
 ##                   get method is used to retrieve the inverse of the matrix if already exists in the cache
 
+##Examples of how to use makeCacheMatrix and cacheSolve
+##Example 1 - 2 by 2 matrix
+##matrix1<-matrix(c(1,2,3,4),2,2)  # creates 2 by 2 matrix
+##matrix1Object<-makeCacheMatrix(matrix1)
+##cacheSolve(matrix1Object)
+##Example 2 - 3 by 3 matrix
+##matrix2<-matrix(c(1,2,3,4,5,6,7,8,10),3,3)  # creates 3 by 3 matrix
+##matrix2Object<-makeCacheMatrix(matrix2)
+##cacheSolve(matrix2Object)
+
+
+
 makeCacheMatrix <- function(x = matrix()) {
  m <- NULL
         set <- function(y) {
