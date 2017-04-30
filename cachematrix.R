66db89ada93d4cd6ffde43a68ba63b7e063ebbfe
alijinnah <alij@blinds.com>
2017-04-30 17:35:40 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCahceMatrix takes an matrix as an argument (assumes it follows the rules such as being square matrix and is invertible) and returns an object with get and set methods for that matrix
+## an object with the set and get method for the matrix. The Set method simply calls the R function to calculate the inverse of the matrix
+## called 'solve' 
+## After makeCahceMatrix is called, the cacheSolve function can be called to cache the inverse of the matrix when called
+## for the first time and to retrieve the cached value on subsequent calls
 
-## Write a short comment describing this function
+## makeCacheMatrix returns set and get methods for a matrix. Set calls the R method 'solve' to calculate the inverse of the matrix
+##                   get method is used to retrieve the inverse of the matrix if already exists in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
  m <- NULL
@@ -21,7 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes the object returned by makeCacheMatrix as the argument. Purpose is to return 
+## the inverse of the matrix from the cache if already exists, and call 'solve' by accessing the makeCacheMatrix object set 
+## method and store the inverse of the matrix in cache. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
